bbc9888cb7b2549df899ee446ff8e1157fba5e85
zhangxiaokang1234 <2298308433@qq.com>
2023-09-15 11:44:56 +0800

    不不不

--- src/main/java/cn/lzy/web/functionextensionmvc/Servlet/MyFilter.java
+++ src/main/java/cn/lzy/web/functionextensionmvc/Servlet/MyFilter.java
@@ -19,7 +19,7 @@ public class MyFilter implements Filter {
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
-        System.out.println("hello MyFilter");
+              System.out.println("hello MyFilter");
 //        filterChain.doFilter(servletRequest,servletResponse);
     }
 
